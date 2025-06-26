c140a5ef0545fde1e6d9e06a2e7a491facda1063
Angel Maldonado <151583275+Anng3l@users.noreply.github.com>
2025-06-26 16:51:12 -0500

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 
+
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -7,8 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+    
+
     // TODO: implement build
-    const String appTitle = "Flutter aplicación 3";
+    const String appTitle = "Blog";
 
     return MaterialApp(
       title: appTitle,
@@ -36,6 +42,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+class BlogEntryCard extends StatelessWidget {
+  const BlogEntryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return 
+
+
+  }
+}
 
 class TitleSection extends StatelessWidget {
   const TitleSection({
@@ -103,18 +121,6 @@ class ButtonSection extends StatelessWidget {
 }
 
 
-/*
-return Padding(
-  padding: EdgeInsets.only(top: 4, bottom: 4),
-  child: Row(
-    children: [
-      ButtonWithText(color: Colors.black, icon: Icons.call, label: "CALL"),
-      ButtonWithText(color: Colors.black, icon: Icons.route, label: "ROUTE"),
-      ButtonWithText(color: Colors.black, icon: Icons.share, label: "SHARE"),
-    ],
-  ),
-);
-*/
 
 
 
@@ -188,157 +194,3 @@ class ImageSection extends StatelessWidget {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-//1
-//Imagen
-
-//2
-//Row 1
-class TitleSection extends StatelessWidget 
-{
-  //Constructor
-  const TitleSection({super.key, required this.name, required this.location});
-
-  final String name;
-  final String location;
-
-  @override
-  Widget build(BuildContext context) {
-
-
-    //Estilizado con el padding 
-    return Padding(
-      padding: const EdgeInsets.all(32),
-      //Row 1
-      child: Row(
-
-        //Columna con 2 hijos. Texto en ambos.
-        children: [
-          Expanded(
-            //1
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                //2
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 32),
-                  child: Text(
-                    name,
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Text(location, style: TextStyle(color: Colors.grey[500])),
-              ],
-            ),
-          ),
-
-          //Íconos y texto, uno junto al otro, junto a la columna de textos.
-          /*3
-          
-          Elementos*/
-          
-          //1. Ícono
-          Icon(Icons.star, color: Colors.red[500]),
-          //2. Texto
-          const Text('41'),
-        ],
-      ),
-    );
-  }
-}
-
-
-
-
-//3
-//Row 2
-class ButtonSection extends StatelessWidget {
-  const ButtonSection ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final Color color = Theme.of(context).primaryColor;
-
-    return SizedBox(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          ButtonWithText(color:color, icon:Icons.call, label: "CALL"),
-          ButtonWithText(color:color, icon:Icons.route, label: "ROUTE"),
-          ButtonWithText(color:color, icon:Icons.share, label: "SHARE")
-        ],        
-      ),
-    );
-  }
-}
-
-class ButtonWithText extends StatelessWidget {
-  const ButtonWithText(
-    {
-      super.key,
-      required this.color,
-      required this.icon,
-      required this.label,  
-    }
-  );
-
-  final Color color;
-  final IconData icon;
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
-*/
